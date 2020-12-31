2462c45b3b55908ea7b0eea8005b8ca7ceb1ae0d
Vivek <me.vpatial@gmail.com>
2020-12-31 18:56:45 +0530

    Changed the App.js Struct.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import classes from "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import Chat from "./components/Chat/Chat";
 
 class App extends Component {
   render() {
-    return <div className={classes.App}>App</div>;
+    return (
+      <div className={classes.App}>
+        <Router>
+          <Switch>
+            <Route path="/" component={Chat} exact />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Auth/Login/Login";
-import Register from "./components/Auth/Register/Register";
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
-ReactDOM.render(<Root />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
