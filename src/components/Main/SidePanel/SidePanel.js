28bcb18df5fe030d540e82b73ad74a5199527e8a
Vivek <me.vpatial@gmail.com>
2021-01-03 13:09:13 +0530

    avatar and name added to sidebar

--- src/components/Main/SidePanel/SidePanel.js
+++ src/components/Main/SidePanel/SidePanel.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import classes from "./SidePanel.css";
 import firebase from "../../../firebase";
 export class SidePanel extends Component {
@@ -12,17 +13,23 @@ export class SidePanel extends Component {
       .catch((err) => console.log(err));
   };
   render() {
+    console.log(this.props.avatar);
     return (
       <div className={classes.SidePanel}>
         <h2>Slack-Clone</h2>
-        {/* <div>
-          <img src={} />
-          <p>Welcome! {}</p>
-        </div> */}
+        <div>
+          <img src={this.props.avatar} alt="avatar" />
+          <p>Welcome! {this.props.username}</p>
+        </div>
         <button onClick={this.signOutHandler}>Sign Out!</button>
       </div>
     );
   }
 }
-
-export default SidePanel;
+const mapStateToProps = (state) => {
+  return {
+    username: state.user.currentUser.displayName,
+    avatar: state.user.currentUser.photoURL,
+  };
+};
+export default connect(mapStateToProps)(SidePanel);
