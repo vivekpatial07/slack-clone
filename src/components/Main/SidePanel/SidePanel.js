41484edb0669c69c2ff6d791cd3eaf1996211c4c
Vivek <me.vpatial@gmail.com>
2021-01-16 15:28:35 +0530

    Styling improved

--- src/components/Main/SidePanel/SidePanel.js
+++ src/components/Main/SidePanel/SidePanel.js
@@ -5,7 +5,6 @@ import firebase from "../../../firebase";
 import Channels from "./Channels/Channels";
 import Button from "@material-ui/core/Button";
 import CodeRoundedIcon from "@material-ui/icons/CodeRounded";
-import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import DirectMessage from "./Channels/DirectMessage/DirectMessage";
@@ -28,19 +27,30 @@ class SidePanel extends Component {
           {/* <Icon>CodeIcon</Icon> */}
           <Typography>Slack-Clone</Typography>
         </h2>
-        <Box
-          p={7}
-          mx="auto"
-          // display="block"
-          display="flex"
-          justifyContent="center"
+        <div
+          style={{
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
         >
           {/* need to restyle  */}
-          <Avatar alt="avatar" src={this.props.avatar} />
+          <img
+            style={{
+              margin: "auto",
+              height: "27px",
+              width: "27px",
+              borderRadius: "27px",
+            }}
+            alt="avatar"
+            src={this.props.avatar}
+          />
           <Typography>{this.props.username}</Typography>
-        </Box>
+        </div>
 
         <Button
+          size="small"
           variant="contained"
           onClick={this.signOutHandler}
           color="secondary"
